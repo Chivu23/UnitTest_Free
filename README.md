c97a3f20952195b7c4cbfc7eef015dcfc0d109ae
Chivu23 <chivu1312g@gmail.com>
2024-01-18 14:47:05 +0200

    test_with_UnitTEST_library

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# UnitTest_Free
-### Library that supports the creation of directly runnable tests inside the classes
+# Some practice with unittest library
+## UnitTest_Free
+
+Library that supports the creation of directly runnable tests inside the classes
+
+## How to use UNITTEST:
+1. import library: import unittest
+2. define a class to contain the tests; the name of this class is important to start with Test and inherit from unittest.TestCase 
+3. inside the tests class, we will have access to two special methods:
+   - setUp() - here define all the activities that must be performed BEFORE ANY TEST in the respective class. 
+   - tearDown() - here define all the activ that must be performed AFTER ANY TEST from  the respective class.
+4. define tests : each test is a method of the respective class
+- To skip a unittest use "@unittest.skip" after that test
+- Running all test files from the terminal: "-m unittest"
 
 
 
-Some practice with unittest library
--- library_unittest.py
+++ library_unittest.py
@@ -6,10 +6,15 @@ https://the-internet.herokuapp.com/login
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class TestLoginPage(unittest.TestCase):
     LINK = "https://the-internet.herokuapp.com/login"
+    BUTTON_LOGIN = (By.XPATH, "//button[@class='radius']")
+    USERNAME = (By.ID, "username")
+    PASSWORD = (By.ID, "password")
+    ERROR_MSG = (By.ID, "flash")
 
     def setUp(self):
         """
@@ -46,6 +51,7 @@ class TestLoginPage(unittest.TestCase):
 
     # TEST - 1
     # - check the URL is correct
+    # @unittest.skip
     def test_current_url(self):
         print("RUNNING TEST")
         actual_url = self.driver.current_url
@@ -56,9 +62,30 @@ class TestLoginPage(unittest.TestCase):
 
     # TEST - 2
     # - Check that the Login button is displayed on the site
+    def test_button_login_is_displayed(self):
+        button_login_elem = self.driver.find_element(*self.BUTTON_LOGIN)      # * args  &   ** kwargs
+        self.assertTrue(button_login_elem.is_displayed(), "This button is not displayed")
 
     # TEST - 3
     # - Check the Login when username is incorrect and pass is correct
+    def test_login_when_username_is_incorrect(self):
+        # insert incorrect username
+        username_elem = self.driver.find_element(*self.USERNAME)
+        username_elem.send_keys("John")
 
+        # pass correct
+        pass_elem = self.driver.find_element(*self.PASSWORD)
+        pass_elem.send_keys("SuperSecretPassword!")
+
+        # click login
+        button_login_elem = self.driver.find_element(*self.BUTTON_LOGIN)
+        button_login_elem.click()
+
+        # check the error msg
+        error_msg_elem = self.driver.find_element(*self.ERROR_MSG)
+        actual_error = error_msg_elem.text
+        print("__________")
+        print(actual_error)
+        self.assertIn("Your username is invalid!", actual_error)
     # TEST - 4
     #
